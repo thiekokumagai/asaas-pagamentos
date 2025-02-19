51d367dcc0ea96801df2c12c4127a4b95730e1e1
Thieko kumagai <thieko.kumagai@gmail.com>
2025-02-19 17:41:31 -0400

    feat: assas

--- asaas-pagamentos.php
+++ asaas-pagamentos.php
@@ -266,7 +266,7 @@ function asaas_webhook_callback(WP_REST_Request $request) {
             }
             
             if ($vencido) {
-                marcar_post_como_rascunho($post_id, $cupom_desconto);
+                marcar_post_como_rascunho($post_id);
             }
         }
     }
@@ -291,8 +291,9 @@ function atualizar_post_pago($post_id, $options, $status, $forma_pagamento, $dat
     }
 }
 
-function marcar_post_como_rascunho($post_id, $cupom_desconto) {
-    wp_update_post(['ID' => $post_id, 'post_status' => 'draft']);
+function marcar_post_como_rascunho($post_id) {
+    wp_update_post(['ID' => $post_id, 'post_status' => 'draft']);    
+    
 }
 
 
